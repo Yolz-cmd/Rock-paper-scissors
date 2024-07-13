0a574d3daa8e3adc56411cc6f2464b98b1cf6385
Yolanda <yolanda.sithole1@gmail.com>
2024-07-13 23:51:54 +0200

    Create get human Choice function

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function getComputerChoice()
 { rps = ['Rock','Paper', 'Scissors'];
 
 let random = Math.floor(Math.random()*rps.length);
-return console.log (rps[random]);
+return console.log ("Computer Choice : " + rps[random]);
 
 }
 getComputerChoice();
+
+function getHumanChoice()
+{
+  let humanChoice = prompt ("Enter your choice of the game ?");
+  return console.log ("Human Choice:" + humanChoice);
+}
+getHumanChoice();
