eceb1de4055fc8432e18395203099bf5a4a64c0b
Yolanda <yolanda.sithole1@gmail.com>
2024-07-14 22:50:22 +0200

    Add for loop to loop game 5 times

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
 function getComputerChoice()
-{ rps = ['Rock','Paper', 'Scissors'];
+{ const rps = ['Rock','Paper', 'Scissors'];
 
-let random = Math.floor(Math.random()*rps.length);
-return console.log ("Computer Choice : " + rps[random]);
+const random = Math.floor(Math.random()*rps.length);
+return  rps[random];
 
 }
-getComputerChoice();
+
 
 function getHumanChoice()
 {
-  let humanChoice = prompt ("Enter your choice of the game ?");
-  return console.log ("Human Choice:" + humanChoice);
+  let human = prompt ("Enter your choice of the game ?");
+  return human;
 }
-getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice)
+{
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+let output;
+  if (humanChoice === computerChoice)
+  {
+ output = "Its a draw";
+}
+else if 
+  ((humanChoice==='rock'&& computerChoice==='scissors')||
+(humanChoice==='scissors'&& computerChoice==='paper')
+ ||(humanChoice==='paper'&& computerChoice==='rock'))
+{
+
+humanScore++;
+output = "You win this round";
+}
+else
+{
+ 
+ computerScore++;
+ output = "Computer won this round"
+}
+console.log(output);
+}
+
+
+ 
+
+  for (let i = 1; i<=5;i++)
+  {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(humanSelection);
+    console.log(computerSelection);
+    playRound(humanSelection,computerSelection);
+ console.log ("Human Score: "+ humanScore);
+  console.log ("Computer Score: "+ computerScore);
+  console.log("----------------------------------------------");
+  }
+  if (humanScore > computerScore)
+  {
+    console.log('YOU BEAT THE COMPUTER');
+  }
+  else { console.log ('the computer won')}
+
+
